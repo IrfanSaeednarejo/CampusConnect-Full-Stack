f6119ec23b6ac514e2a386c1e54d090a9e5e752b
Irfan Saeed <irfansaeed.narejo@gmail.com>
2026-03-23 20:22:27 +0500

    feat: introduce User Mongoose model with authentication, profile, academic, and preferences schemas, and create study group controller.

--- server/src/controllers/studyGroup.controller.js
+++ server/src/controllers/studyGroup.controller.js
@@ -1,3 +1,3 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model.js";
+import { StudyGroup } from "../models/studyGroup.model.js";
